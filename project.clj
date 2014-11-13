efc05111bb8d1b03d76b3f444259abc80dbd7ed1
supermomonga <hi@supermomonga.com>
2014-11-13 15:44:55 +0900

    bukkit is ok

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject rukkit "1.0.0-SNAPSHOT"
   :description "Awesome super benri cool plugin"
-  :dependencies [[com.supermomonga/craftbukkit "1.7.9-R0.2"]
+  :dependencies [[org.bukkit/bukkit "1.7.9-R0.2"]
                  [org.jruby/jruby-complete "1.7.16.1"]]
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :repositories [["craftbukkit" "file:craftbukkit"]]
+  :repositories {"org.bukkit"
+                 "http://repo.bukkit.org/content/groups/public/"}
   :javac-options ["-d" "classes/" "-Xlint:deprecation"]
   :java-source-paths ["java"])
