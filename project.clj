a63968dbdd61b2bf86cddadf1ebf4a4a0537692c
supermomonga <hi@supermomonga.com>
2014-11-13 15:28:22 +0900

    local repo

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject rukkit "1.0.0-SNAPSHOT"
   :description "Awesome super benri cool plugin"
-  :dependencies [[org.bukkit/bukkit "1.7.9-R0.2"]
+  :dependencies [[com.supermomonga/craftbukkit "1.7.9-R0.2"]
                  [org.jruby/jruby-complete "1.7.16.1"]]
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :repositories {"org.bukkit"
-                 "http://repo.bukkit.org/content/groups/public/"}
+  :repositories [["craftbukkit" "file:craftbukkit"]]
   :javac-options ["-d" "classes/" "-Xlint:deprecation"]
   :java-source-paths ["java"])
