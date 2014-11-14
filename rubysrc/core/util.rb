8a341a7ce4a31456906aa32a6ce2b16c0688b600
ujihisa <ujihisa@gmail.com>
2014-11-13 20:38:02 -0800

    later is majibenri

--- rubysrc/core/util.rb
+++ rubysrc/core/util.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+import 'org.bukkit.Bukkit'
 
 module Util
   extend self
@@ -7,4 +8,12 @@ module Util
     Bukkit.server.broadcast_message messages.join
   end
 
+  def sec(n)
+    (n * 20).to_i
+  end
+
+  def later(tick, &block)
+    plugin = Bukkit.plugin_manager.plugin("rukkit")
+    Bukkit.getScheduler.scheduleSyncDelayedTask(plugin, block, tick)
+  end
 end
