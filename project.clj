9bf52e881e5fe4f1a8d6a45f517ae51b2dc670be
supermomonga <hi@supermomonga.com>
2014-11-15 19:26:35 +0900

    use 1.7.9-R0.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rukkit "1.0.0-SNAPSHOT"
   :description "Awesome super benri cool plugin"
-  :dependencies [[org.bukkit/bukkit "1.7.9-R0.2"]
+  :dependencies [[org.bukkit/bukkit "1.7.9-R0.1"]
                  [org.jruby/jruby-complete "1.7.16.1"]]
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
