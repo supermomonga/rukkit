117663dab302a86ebbecfaf6c71f7328624704c3
ujihisa <ujihisa@gmail.com>
2014-11-15 23:35:40 -0800

    superjump

---
+++ example_plugins/super_jump.rb
@@ -0,0 +1,32 @@
+import 'org.bukkit.Sound'
+require_resource 'scripts/util'
+
+module SuperJump
+  def on_player_toggle_sneak(evt)
+    player = evt.player
+    return unless %w[world world_nether].include?(player.location.world.name)
+
+    name = player.name
+    @crouching_counter ||= {}
+    @crouching_counter[name] ||= 0
+    @crouching_countingdown ||= false
+    if evt.sneaking?
+      # counting up
+      @crouching_counter[name] += 1
+      Rukkit::Uti.later Rukkit::Uti.sec(2.0) do
+        @crouching_counter[name] -= 1
+      end
+      if @crouching_counter[name] == 4
+        play_sound(add_loc(player.location, 0, 5, 0), Sound::BAT_TAKEOFF, 1.0, 0.0)
+        # evt.player.send_message "superjump!"
+        player.fall_distance = 0.0
+        player.velocity = player.velocity.tap {|v| v.set_y jfloat(1.4) }
+      end
+    end
+  end
+
+  # TODO move it to Util
+  def jfloat(rubyfloat)
+    rubyfloat.to_java Java.float
+  end
+end
