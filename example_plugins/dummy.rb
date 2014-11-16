aca7a66f6994eab29e1646e5fb681ab4002df083
ujihisa <ujihisa@gmail.com>
2014-11-16 01:12:28 -0800

    succeeded

--- example_plugins/dummy.rb
+++ example_plugins/dummy.rb
@@ -5,10 +5,10 @@ module Dummy
   extend self
   extend Rukkit::Util
 
-  def on_player_toggle_sneak(evt)
-    player = evt.player
-    if player.name == 'ujm'
-      play_sound(add_loc(player.location, 0, 5, 0), Sound.values.to_a.sample, 1.0, 0.0)
-    end
-  end
+  # def on_player_toggle_sneak(evt)
+  #   player = evt.player
+  #   if player.name == 'ujm'
+  #     play_sound(add_loc(player.location, 0, 5, 0), Sound.values.to_a.sample, 1.0, 0.0)
+  #   end
+  # end
 end
