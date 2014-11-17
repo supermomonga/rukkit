7d062f7aab33f690f533a776ae0a32796e32370e
supermomonga <hi@supermomonga.com>
2014-11-17 09:50:51 +0900

    prepare to make it rukkit script

--- example_plugins/chat_relay.rb
+++ example_plugins/chat_relay.rb
@@ -7,7 +7,10 @@ require_resource 'scripts/util'
 import 'org.bukkit.entity.Player'
 import 'org.bukkit.ChatColor'
 
-module Lingr
+# test
+puts defined?(Lingr) ? "lingr library found" : "lingr library not found"
+
+module ChatRelay
   ROMAJI_CONVERSION_TABLE = { # {{{
     # four charcters
     xtsu: 'っ',
---
+++ example_scripts/lingr.rb
@@ -0,0 +1,10 @@
+
+module Lingr
+
+  extend self
+
+  def test
+    puts :this_is_the_test
+  end
+
+end
