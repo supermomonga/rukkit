e3e4dc4a190712035e06a03abfd7465810148210
ujihisa <ujihisa@gmail.com>
2014-11-17 19:58:53 -0800

    it waits for input

--- example_plugins/dummy.rb
+++ example_plugins/dummy.rb
@@ -23,7 +23,7 @@ module Dummy
 
       # just for now
       Dir.chdir('/home/rukkit/rukkit') do
-        system 'git pull --rebase'
+        system 'git pull --rebase', stdin: '/dev/null'
         Bukkit.dispatch_command(sender, 'reload')
       end
     when ['rukkit', 'eval']
