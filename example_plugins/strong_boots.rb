d73b9ea857c9b31cd81e5d81b4ee90977f16aec1
ujihisa <ujihisa@gmail.com>
2014-11-17 17:31:33 -0800

    workinprogress

--- example_plugins/strong_boots.rb
+++ example_plugins/strong_boots.rb
@@ -1,4 +1,20 @@
+import 'org.bukkit.Sound'
+import 'org.bukkit.entity.Player'
+require_resource 'scripts/util'
 
+module StrongBoots
+  extend self
+  extend Rukkit::Util
 
-module Hi
+  def on_entity_damage_by_block(evt)
+    log.debug("on_entity_damage_by_block: #{evt}")
+
+    case evt.entity
+    when Player
+      b = evt.entity.inventory.boots or return
+
+      # just for now
+      evt.entity.send_message("[DEBUG] you have #{b}")
+    end
+  end
 end
