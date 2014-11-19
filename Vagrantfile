b09455028ef3c42626eff1c028e7f99a86d76cd4
supermomonga <hi@supermomonga.com>
2014-11-19 19:18:05 +0900

    works?

--- Vagrantfile
+++ Vagrantfile
@@ -5,13 +5,12 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  config.vm.define 'rukkit' do |c|
+  config.vm.define 'rukkit-vagrant' do |c|
     c.vm.provider "virtualbox" do |vb|
       vb.customize ["modifyvm", :id, "--memory", "1024"]
     end
-    c.vm.box = "ubuntu_1404_amd64"
+    c.vm.box = "ffuenf/debian-7.6.0-amd64"
     c.vm.box_check_update = true
-    c.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
     c.vm.network "private_network", ip: "192.168.33.10"
     # c.vm.network "forwarded_port", guest: 80, host: 8080
     c.vm.network "forwarded_port", guest: 25565, host: 25565
@@ -19,19 +18,18 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     # sync rukkit source
     c.vm.synced_folder './', '/home/vagrant/rukkit', owner: 'vagrant', group: 'vagrant'
 
-    # Update packages
-    c.vm.provision :shell, inline: <<-EOC
-cat /etc/apt/sources.list | sed -e 's|http://[^ ]*|mirror://mirrors.ubuntu.com/mirrors.txt|g' > /tmp/sources.list
-if !(diff -q /etc/apt/sources.list /tmp/sources.list); then
-  mv /tmp/sources.list /etc/apt/sources.list
-  apt-get update
-fi
-    EOC
+    # c.ssh.username = 'root'
+    c.ssh.private_key_path = '~/.vagrant.d/insecure_private_key'
 
     # Make sure to use root user
     c.vm.provision :shell, inline: <<-EOC
-    sudo echo 'root:vagrant' | /usr/sbin/chpasswd
+    echo 'root:vagrant' | /usr/sbin/chpasswd
     EOC
+
+    # c.vm.provision :shell, inline: <<-EOC
+    # sudo mkdir /root/.ssh
+    # sudo cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
+    # EOC
   end
 
 end
--- sunzi/install.sh
+++ sunzi/install.sh
@@ -25,26 +25,55 @@ if sunzi.install "sysstat"; then
   /etc/init.d/sysstat restart
 fi
 
-# Set RAILS_ENV
-environment=<%= @attributes.environment %>
+if [[ "$(which java)" != /usr/bin/java ]]; then
+  echo "Installing Java"
+  echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+  echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 
-if ! grep -Fq "RAILS_ENV" ~/.bash_profile; then
-  echo 'Setting up RAILS_ENV...'
-  echo "export RAILS_ENV=$environment" >> ~/.bash_profile
-  source ~/.bash_profile
+  # sunzi.mute "add-apt-repository -y ppa:webupd8team/java"
+  sunzi.mute "apt-get update"
+  echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+  apt-get -y install oracle-java8-installer
+  apt-get -y install oracle-java8-set-default
+  java -version
 fi
 
-# Install Ruby using RVM
-source recipes/rvm.sh
+if [[ "$(which lein)" != /usr/local/bin/lein ]]; then
+  echo "Installing leiningen"
+  sunzi.mute "curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o /usr/local/bin/lein"
+  sunzi.mute "chmod a+x /usr/local/bin/lein"
+  sunzi.mute "lein"
+fi
+
+
+if [ ! -e /home/vagrant/craftbukkit/craftbukkit.jar ]; then
+  echo "Installing craftbukkit"
+  craftbukkit_url=<%= @attributes.craftbukkit_url %>
+  craftbukkit_md5=<%= @attributes.craftbukkit_md5 %>
+  mkdir -p /home/vagrant/craftbukkit
+  sunzi.mute "curl $craftbukkit_url -o /home/vagrant/craftbukkit/craftbukkit.jar"
+  hash=`openssl md5 /home/vagrant/craftbukkit/craftbukkit.jar | awk '{print $2}'`
+  echo "hash: $hash"
+  if [[ "$hash" != "$craftbukkit_md5" ]]; then
+    echo "MD5 hash didn't match. Removing it."
+    rm /home/vagrant/craftbukkit/craftbukkit.jar
+  fi
+fi
+
+# Install Ruby using rbenv
+source recipes/rbenv.sh
 ruby_version=<%= @attributes.ruby_version %>
 
-if [[ "$(which ruby)" != /usr/local/rvm/rubies/ruby-$ruby_version* ]]; then
+if [[ "$(which ruby)" != /usr/local/rbenv/versions/$ruby_version* ]]; then
   echo "Installing ruby-$ruby_version"
   # Install dependencies using RVM autolibs - see https://blog.engineyard.com/2013/rvm-ruby-2-0
-  rvm install --autolibs=3 $ruby_version
-  rvm $ruby_version --default
+  rbenv install $ruby_version
+  rbenv global $ruby_version
   echo 'gem: --no-ri --no-rdoc' > ~/.gemrc
 
   # Install Bundler
   gem install bundler
 fi
+
+echo "Provisioning finished."
