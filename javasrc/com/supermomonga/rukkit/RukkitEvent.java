f4c7a14957f711496a288eff324f14b35cb27748
kamichidu <c.kamunagi@gmail.com>
2014-12-01 11:53:53 +0900

    Documents

--- javasrc/com/supermomonga/rukkit/RukkitEvent.java
+++ javasrc/com/supermomonga/rukkit/RukkitEvent.java
@@ -9,13 +9,30 @@ import org.bukkit.event.Event;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * The Rukkit event.
+ *
+ * This has Bukkit's event class and Rukkit event name (for Java and Ruby).
+ *
+ * @author kamichidu
+ */
 public class RukkitEvent {
+  /** Bukkit's event class which is related this Rukkit's event */
   private final Class<?> eventClass;
 
+  /** The name of this event for Java side. */
   private final String javaEventName;
 
+  /** The name of this event for Ruby side. */
   private final String rubyEventName;
 
+  /**
+   * Constructs {@link RukkitEvent} object by Bukkit's event class.
+   *
+   * @param eventClass Bukkit's event class
+   * @throws NullPointerException When eventClass is null
+   * @throws IllegalArgumentException When eventClass is not a subclass of {@link org.bukkit.event.Event}
+   */
   public RukkitEvent(Class<?> eventClass) {
     checkNotNull(eventClass);
     checkArgument(Event.class.isAssignableFrom(eventClass));
@@ -27,14 +44,32 @@ public class RukkitEvent {
     this.rubyEventName= CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, this.javaEventName);
   }
 
+  /**
+   * Returns a class object which is subclass of {@link org.bukkit.event.Event}.
+   * Never returns null value.
+   *
+   * @return The class object
+   */
   public Class<?> getEventClass() {
     return this.eventClass;
   }
 
+  /**
+   * Returns an event name for Java.
+   * Never returns null value.
+   *
+   * @return The event name
+   */
   public String getJavaEventName() {
     return this.javaEventName;
   }
 
+  /**
+   * Returns an event name for Java.
+   * Never returns null value.
+   *
+   * @return The event name
+   */
   public String getRubyEventName() {
     return this.rubyEventName;
   }
