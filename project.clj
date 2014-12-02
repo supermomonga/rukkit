e5960c351125706204e6001cc698d55e09f5f1ac
raa0121 <raa0121@gmail.com>
2014-12-02 12:52:41 +0900

    add jmockit 1.13

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.bukkit/bukkit "1.7.9-R0.1"]
                  [org.jruby/jruby-complete "1.7.16.1"]
                  [org.javassist/javassist "3.18.2-GA"]
-                 [com.google.guava/guava "18.0"]]
+                 [com.google.guava/guava "18.0"]
+                 [org.jmockit/jmockit "1.13"]]
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :repositories {"org.bukkit"
