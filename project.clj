ddd8f2e9f1edd29b32f72315c98636a55dc8f13a
raa0121 <raa0121@gmail.com>
2014-12-02 13:01:13 +0900

    add profile dev

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :dependencies [[org.bukkit/bukkit "1.7.9-R0.1"]
                  [org.jruby/jruby-complete "1.7.16.1"]
                  [org.javassist/javassist "3.18.2-GA"]
-                 [com.google.guava/guava "18.0"]
-                 [org.jmockit/jmockit "1.13"]]
+                 [com.google.guava/guava "18.0"]]
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :repositories {"org.bukkit"
@@ -12,4 +11,5 @@
   :javac-target "1.8"
   :javac-source "1.8"
   :javac-options ["-d" "classes/" "-Xlint:all"]
-  :java-source-paths ["javasrc"])
+  :java-source-paths ["javasrc"]
+  :profiles {:dev {:dependencies [[org.jmockit/jmockit "1.13"]]}})
