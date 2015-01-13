e3bd5156a9f48887b1674bcfa8e1fe092b6cafc4
raa0121 <raa0121@gmail.com>
2015-01-13 23:57:40 +0900

    add jedis

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.bukkit/bukkit "1.7.9-R0.2"]
                  [org.jruby/jruby-complete "1.7.16.1"]
                  [org.javassist/javassist "3.18.2-GA"]
+                 [redis.clients/jedis "2.6.0"]
                  [com.google.guava/guava "18.0"]]
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
