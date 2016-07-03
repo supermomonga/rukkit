b9b56b54d64ba170077709075d6f400a5e6a07bc
supermomonga <hi@supermomonga.com>
2016-07-03 22:30:45 +0900

    add clojure dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject rukkit "1.0.0-SNAPSHOT"
   :description "Awesome super benri cool plugin"
-  :dependencies [[org.spigotmc/spigot-api "1.10-R0.1-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.spigotmc/spigot-api "1.10-R0.1-SNAPSHOT"]
                  [org.spigotmc/spigot "1.10"]
                  [org.jruby/jruby-complete "9.1.2.0"]
                  [org.javassist/javassist "3.18.2-GA"]
